d7e76fead9875aef7c6b6a9cd7e3f2b5cc2759c1
Jaime A <jaimeangel1097@gmail.com>
2024-06-20 10:52:07 -0500

    corrigiendo codigo con excepciones para dia viernes

--- script.py
+++ script.py
@@ -9,68 +9,62 @@ meses_es = {
     '09': 'SEPTIEMBRE', '10': 'OCTUBRE', '11': 'NOVIEMBRE', '12': 'DICIEMBRE'
 }
 
-# Obtener la fecha actual
-hoy = datetime.datetime.today()
+# Función para procesar archivos de viernes, incluyendo sábado y domingo (y lunes si es festivo)
+def procesar_archivos_vienes(fecha):
+    yyyy = fecha.strftime('%Y')
+    mm_number = fecha.strftime('%m')
+    mm_palabra = meses_es[mm_number]
+    dd = fecha.strftime('%d')
+    yyyymmdd = fecha.strftime('%Y%m%d')
 
-# Definir el año, mes y día actuales
-yyyy = hoy.strftime('%Y')
-mm_number = hoy.strftime('%m')
-mm_palabra = meses_es[mm_number]
-dd = hoy.strftime('%d')
+    ruta_precios = f'Controles Diarios/{yyyy}/Controles KPS/{mm_number} {mm_palabra}/{dd}/Precios'
+    subcarpetas = sorted([d for d in os.listdir(ruta_precios) if os.path.isdir(os.path.join(ruta_precios, d)) and len(d) == 4 and d.isdigit()])
 
-# Formatear la fecha para los nombres de archivo
-yyyymmdd = hoy.strftime('%Y%m%d')
+    for subcarpeta in subcarpetas:
+        ruta_origen = os.path.join(ruta_precios, subcarpeta, 'Archivos Originales')
+        procesar_archivos_genericos(ruta_origen, yyyymmdd, subcarpeta == subcarpetas[-1])
 
-# Ruta de origen y destino
-ruta_origen = f'Controles Diarios/{yyyy}/Controles KPS/{mm_number} {mm_palabra}/{dd}/Precios/Archivos Originales'
-ruta_destino = 'Controles Backup/Controles KPS/Archivos Originales'
+# Función para procesar archivos de lunes a jueves
+def procesar_archivos_semana(fecha):
+    yyyy = fecha.strftime('%Y')
+    mm_number = fecha.strftime('%m')
+    mm_palabra = meses_es[mm_number]
+    dd = fecha.strftime('%d')
+    yyyymmdd = fecha.strftime('%Y%m%d')
+    ruta_origen = f'Controles Diarios/{yyyy}/Controles KPS/{mm_number} {mm_palabra}/{dd}/Precios/Archivos Originales'
 
-# Crear la carpeta de destino si no existe
-os.makedirs(ruta_destino, exist_ok=True)
+    procesar_archivos_genericos(ruta_origen, yyyymmdd,True)
 
-# Diccionario para los nuevos nombres de archivo
-nombres_archivos = {
-    '900408537_cva_alloc': f'{yyyymmdd}_cvadva.txt',
-    'Fwd_USDCOP_Diaria': f'{yyyymmdd}_Fwd_USDCOP.txt',
-    'Matriz_TC': f'{yyyymmdd}_Mat_Monedas.txt',
-    'SwapCC_IBR_Diaria': f'{yyyymmdd}_LIBORCOP.txt',
-    'SB': f'{yyyymmdd}_Betas.txt',
-    'SP': f'{yyyymmdd}_Precios.txt',
-    'SV': f'{yyyymmdd}_Tasas.txt'
-}
+# Función genérica para copiar y renombrar archivos
+def procesar_archivos_genericos(ruta_origen, yyyymmdd, es_ultimo_dia=False):
+    ruta_destino = 'Controles Backup/Controles KPS/Archivos Originales'
+    os.makedirs(ruta_destino, exist_ok=True)
 
-# Función para cambiar el nombre de los archivos .001
-def cambiar_nombre_archivo_001(nombre_original):
-    tipo = nombre_original[:2]
-    if tipo in ['SB', 'SP', 'SV']:
-        nuevo_nombre = nombres_archivos[tipo]
-        return nuevo_nombre
-    return None
+    nombres_archivos = {
+        '900408537_cva_alloc': f'{yyyymmdd}_cvadva.txt',
+        'Fwd_USDCOP_Diaria': f'{yyyymmdd}_Fwd_USDCOP.txt',
+        'Matriz_TC': f'{yyyymmdd}_Mat_Monedas.txt',
+        'SwapCC_IBR_Diaria': f'{yyyymmdd}_LIBORCOP.txt',
+        'SB': f'{yyyymmdd}_Betas.txt',
+        'SP': f'{yyyymmdd}_Precios.txt',
+        'SV': f'{yyyymmdd}_Tasas.txt'
+    }
 
-# Copiar y renombrar archivos
-for archivo in os.listdir(ruta_origen):
-    nombre_sin_extension, extension = os.path.splitext(archivo)
-    
-    if extension == '.txt':
-        # Extraer la parte del nombre sin la fecha
-        nombre_base = '_'.join(nombre_sin_extension.split('_')[:-1])
+    for archivo in os.listdir(ruta_origen):
+        nombre_base, extension = os.path.splitext(archivo)
+        nombre_base = '_'.join(nombre_base.split('_')[:-1])
         if nombre_base in nombres_archivos:
             nuevo_nombre = nombres_archivos[nombre_base]
-        else:
-            continue
-    elif extension == '.001':
-        nuevo_nombre = cambiar_nombre_archivo_001(nombre_sin_extension + extension)
-        if nuevo_nombre:
-            nuevo_nombre = nuevo_nombre.replace('.001', '.txt')
-        else:
-            continue
-    else:
-        continue
-    
-    ruta_archivo_origen = os.path.join(ruta_origen, archivo)
-    ruta_archivo_destino = os.path.join(ruta_destino, nuevo_nombre)
-    
-    # Copiar el archivo de origen al destino con el nuevo nombre
-    shutil.copy2(ruta_archivo_origen, ruta_archivo_destino)
+            ruta_archivo_origen = os.path.join(ruta_origen, archivo)
+            ruta_archivo_destino = os.path.join(ruta_destino, nuevo_nombre)
+            shutil.copy2(ruta_archivo_origen, ruta_archivo_destino)
+            if nombre_base == 'Matriz_TC' and es_ultimo_dia:
+                ruta_extra = os.path.join(ruta_destino, 'Mat_Monedas.txt')
+                shutil.copy2(ruta_archivo_origen, ruta_extra)
 
-print("Archivos copiados y renombrados exitosamente.")
+# Obtener la fecha actual y determinar el día de la semana
+hoy = datetime.datetime.today()
+if hoy.weekday() == 4:  # Viernes
+    procesar_archivos_vienes(hoy)
+else:
+    procesar_archivos_semana(hoy)
